d5b756262429a632e396c962c59499fe803c9ea9
techqurater <techqurater@gmail.com>
2022-05-29 20:35:53 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express();
+const PORT = process.env.port || 5000
+
+app.get('/', (req, res) => {
+    res.send("<h1>Hello World !!</h1>")
+})
+
+app.get('/api', (req, res) => {
+    res.json({name:"John Doe", email:"johndoe@domainname.com", phone:"9999999999"})
+})
+app.listen(PORT, () => {
+    console.log(`Application is listning the port ${PORT}`);
+})
