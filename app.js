b18cf9510a9c39397ba722c0fe197c704e417cca
techqurater <techqurater@gmail.com>
2022-05-29 20:47:29 +0530

    text change

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
-    res.send("<h1>Hello World !!</h1>")
+    res.send("<h1>Hello World</h1>")
 })
 
 app.get('/api', (req, res) => {
